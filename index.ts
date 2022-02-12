09d546f7fe0c619653c32ccac2464077b217f446
rainy-25Ghz <1748420480@qq.com>
2022-02-12 14:33:06 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import './style.css';
 
 import * as THREE from 'three';
 import { ParametricGeometry } from 'three/examples/jsm/geometries/ParametricGeometry';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 //配置webgl渲染器
 const aspectRatio = window.innerWidth / window.innerHeight;
@@ -22,6 +23,7 @@ const camera = new THREE.OrthographicCamera(
 camera.position.set(200, 200, 200);
 
 const scene = new THREE.Scene();
+//设置几何
 const geometry = new ParametricGeometry(
   (u, v, target) => {
     console.log(u, v, target);
@@ -34,8 +36,10 @@ const geometry = new ParametricGeometry(
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const surface = new THREE.Mesh(geometry, material);
 scene.add(surface);
-
-
-const animate=()=>{
-  renderer.render(scene,camera);
-}
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+const animate = () => {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+};
