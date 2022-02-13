0a1c821fc43cf5d09dc8cd364dca92a02f1c073c
rainy-25Ghz <1748420480@qq.com>
2022-02-13 10:11:47 +0800

    2 files modified

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import './style.css';
 import * as THREE from 'three';
 import { ParametricGeometry } from 'three/examples/jsm/geometries/ParametricGeometry';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Vector3 } from 'three/src/math/Vector3';
 
 //配置webgl渲染器
 const aspectRatio = window.innerWidth / window.innerHeight;
@@ -20,21 +21,60 @@ const camera = new THREE.OrthographicCamera(
   0.1,
   1000
 );
-camera.lookAt(0, 0, 0);
+camera.position.z = -100;
+
 const scene = new THREE.Scene();
+
+const drawCell = (points: number[][]) => {
+  const p0 = points[0]; //左下角
+  const p1 = points[1]; //右下角
+  const p2 = points[2]; //左上角
+  const p3 = points[3]; //右上角
+  const positions = [...p0, ...p1, ...p2, ...p3];
+  const uvs = [...[0, 0], ...[1, 0], ...[0, 1], ...[1, 1]];
+  const geometry = new THREE.BufferGeometry();
+  const positionNumComponents = 3;
+  const uvNumComponents = 2;
+  geometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(
+      new Float32Array(positions),
+      positionNumComponents
+    )
+  );
+  geometry.setAttribute(
+    'uv',
+    new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents)
+  );
+  geometry.computeVertexNormals();
+  geometry.setIndex([0, 1, 2, 2, 1, 3]);
+  const mesh = new THREE.Mesh(
+    geometry,
+    new THREE.MeshLambertMaterial({ color: 0xffee00 })
+  );
+  scene.add(mesh);
+};
+
 //设置几何
-const geometry = new ParametricGeometry(
-  (u, v, target) => {
-    // console.log(u, v, target);
-    //sin(sqrt(a*x^2  + b*y^2))
-    target.set(u, v, Math.sin(Math.sqrt(u * u + v * v)));
-  },
-  25,
-  25
-);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const surface = new THREE.Mesh(geometry, material);
-scene.add(surface);
+// const geometry = new ParametricGeometry(
+//   (u, v, target) => {
+//     // console.log(u, v, target);
+//     //sin(sqrt(a*x^2  + b*y^2))
+//     target.set(u, v, Math.sin(u * v));
+//   },
+//   25,
+//   25
+// );
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const surface = new THREE.Mesh(geometry, material);
+const pts = [
+  [0, 0, 0],
+  [10, 0, 0],
+  [0, 10, 0],
+  [10, 10, 0],
+];
+drawCell(pts);
+// scene.add(surface);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 const animate = () => {
